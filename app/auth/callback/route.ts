588793452358c152a5667ea6c33cace072b6e14a
Rayakoo <rafianantan@gmail.com>
2026-06-19 06:21:13 +0700

    login google

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams, nextUrl } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
-  const origin = nextUrl.origin;
 
   if (code) {
     return NextResponse.redirect(`${origin}${next}?code=${code}`);
